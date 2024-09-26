a6e1d192140d2d8ad96e08a798bac926f2d954c0
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-09-26 16:52:45 -0700

    Replace lorem fillers with actual content and add project images to HomePage and AllProjectsPage

--- src/components/AllProjectsPage.jsx
+++ src/components/AllProjectsPage.jsx
@@ -1,4 +1,9 @@
 import { Link } from 'react-router-dom';
+import membersOnlyImgSrc from '../assets/members-only-main.jpg';
+import inventoryMateImgSrc from '../assets/inventory-mate-main.png';
+import urbanThreadImgSrc from '../assets/urban-thread-main.png';
+import memoryCardImgSrc from '../assets/memory-card-main.png';
+import battleshipImgSrc from '../assets/battleship-main.png';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -12,14 +17,84 @@ const AllProjectsPage = () => (
         back-end technologies and databases.
       </p>
       <article>
-        <h2>Title</h2>
+        <h2>My Personal Blog API</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
-          aliquid repudiandae quae labore animi cupiditate.
+          A full-stack blog platform built with Prisma, PostgreSQL, and
+          Node/Express.js. This project features role-based authentication, a
+          secure comments system, and effective article management. Users can
+          view published articles while the author has the ability to create,
+          edit, and manage content dynamically.
         </p>
         {/* TODO: add screenshot of project */}
       </article>
-      {/* TODO: add more projects */}
+      <article>
+        <h2>Members Only: Exclusive Clubhouse for Anonymous Posts</h2>
+        <p>
+          An Express-based platform designed as an exclusive clubhouse where
+          members can write and share anonymous posts. This project is
+          structured using routes, controllers, and views, focusing on data
+          management with PostgreSQL, user authentication with Passport.js, and
+          secure password handling with bcryptjs.
+        </p>
+        <img
+          src={membersOnlyImgSrc}
+          alt="Digital image of multiple devices displaying chat logs."
+        />
+      </article>
+      <article>
+        <h2>InventoryMate: Simple Inventory Management Application</h2>
+        <p>
+          An inventory management application built with Express and MongoDB,
+          which features site-specific routes, EJS templates for dynamic views,
+          and an MVC architecture to ensure a clear separation of logic. This
+          project enables users to create, read, update, and delete (CRUD)
+          categories and items within a structured interface.
+        </p>
+        <img
+          src={inventoryMateImgSrc}
+          alt="Screenshot of the home page of InventoryMate featuring a navigational sidebar and an overview of the application in the main content."
+        />
+      </article>
+      <article>
+        <h2>Urban Thread: Mock E-Commerce Store</h2>
+        <p>
+          A responsive e-commerce application powered by the FakeStore API,
+          allowing users to browse products and manage items in their cart. This
+          project emphasizes a user-friendly design and delivers a seamless
+          shopping experience similar to that of real-world e-commerce brands.
+        </p>
+        <img
+          src={urbanThreadImgSrc}
+          alt="Screenshot of the home page of Urban Thread featuring a navigation header, promotional banner, and 3 images of models representing the (fake) brand."
+        />
+      </article>
+      <article>
+        <h2>Memory Card Game: Interactive Memory Challenge</h2>
+        <p>
+          An interactive memory card game developed with React to challenge
+          players' memory and recall abilities. This project utilizes hooks for
+          managing state and side effects while fetching card data from the
+          Nintendo Amiibo API, creating a smooth user experience as players
+          attempt to match pairs of cards.
+        </p>
+        <img
+          src={memoryCardImgSrc}
+          alt="Screenshot of the home page of the memory card game featuring a scoreboard at the top and 8 cards with images of different Animal Crossing characters laid out below it."
+        />
+      </article>
+      <article>
+        <h2>Battleship Game</h2>
+        <p>
+          A web-based recreation of the classic Battleship game designed with a
+          simple user interface and interactive gameplay. This project showcases
+          algorithmic thinking, grid-based logic, and player-vs-AI functionality
+          for an engaging user experience.
+        </p>
+        <img
+          src={battleshipImgSrc}
+          alt="Screenshot of the gameplay featuring the player and opponent's game boards with hits and misses."
+        />
+      </article>
       <p>Like what you see? Let's collaborate and bring your ideas to life.</p>
       <Link to="/contact">Let's Connect</Link>
     </main>
