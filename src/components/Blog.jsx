543265e5429ae5a70adec7e46ad15b4b110a3366
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-10-10 16:29:36 -0700

    Create Blog component and handle removal of token from local storage when expiration time is reached

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,23 +1,106 @@
+import { jwtDecode } from 'jwt-decode';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import useFetchAPI from '../hooks/useFetchAPI';
+import { getToken, removeToken } from '../utils/tokenService';
 import Header from './Header';
 import Footer from './Footer';
-import { Link } from 'react-router-dom';
 
-const Blog = () => (
-  <>
-    <Header />
-    <main>
-      <h1>Hannah Kim's Developer Blog</h1>
-      <nav>
-        <Link to="/blog/articles">All Posts</Link>
-        <div>
-          <Link to="/blog/login">Log In</Link>
-          <Link to="/blog/register">Sign Up</Link>
-        </div>
-      </nav>
-      <h2>Articles</h2>
-    </main>
-    <Footer />
-  </>
-);
+const Blog = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [shouldLogout, setShouldLogout] = useState(false);
+
+  const token = getToken();
+  const navigate = useNavigate();
+
+  const url = '/blog/auth/logout';
+  const method = 'POST';
+  const body = {};
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const { data, fetchError } = useFetchAPI(
+    shouldLogout ? url : null,
+    method,
+    body,
+    shouldLogout ? options : null
+  );
+
+  useEffect(() => {
+    if (token === 'undefined') {
+      return;
+    }
+
+    const decodedToken = jwtDecode(token);
+
+    if (decodedToken.exp) {
+      const currentTime = Date.now() / 1000;
+      const timeUntilExp = decodedToken.exp - currentTime;
+
+      if (timeUntilExp <= 0) {
+        handleLogout();
+      } else {
+        const timeoutId = setTimeout(() => {
+          handleLogout();
+        }, timeUntilExp * 1000);
+
+        return () => clearTimeout(timeoutId);
+      }
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (data) {
+      setLoading(false);
+      setError(null);
+      setShouldLogout(false);
+      removeToken();
+      navigate('/blog/auth/login');
+    }
+  }, [data, navigate]);
+
+  useEffect(() => {
+    if (fetchError) {
+      setLoading(false);
+      setError(fetchError);
+      setShouldLogout(false);
+    }
+  }, [fetchError]);
+
+  const handleLogout = () => {
+    setLoading(true);
+    setError(null);
+    setShouldLogout(true);
+  };
+
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Hannah Kim's Developer Blog</h1>
+        <nav>
+          <Link to="/blog/articles">All Posts</Link>
+          <div>
+            {error && <p>{error}</p>}
+            {token && (
+              <button type="button" onClick={handleLogout} disabled={loading}>
+                Log out
+              </button>
+            )}
+            {!token && <Link to="/blog/auth/login">Log In</Link>}
+            {!token && <Link to="/blog/auth/register">Sign Up</Link>}
+          </div>
+        </nav>
+        <h2>Articles</h2>
+        {/* TODO: fetch articles */}
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default Blog;
