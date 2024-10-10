8dbcdc1e5cfc3ab6cd60fc9854b7f73f165588a7
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-10-10 13:51:02 -0700

    Fix incorrect syntax and add default headers in axiosConfig

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.baseUrl = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.headers = {
+  'Content-Type': 'application/json',
+};
 axios.defaults.withCredentials = true;
 
 export default axios;
