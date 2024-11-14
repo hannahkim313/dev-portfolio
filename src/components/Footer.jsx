cbf893b1e503207cd1c4fe1cace2809229733b62
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-11-14 13:10:36 -0800

    Overhaul footer with new layout and styles using a mobile-first approach
    This commit adds an image of the author, the author name, and the author's title to the footer for a touch of personality. Each link in the Get In Touch section was replaced with an icon for a cleaner, minimal look.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,77 @@
 import { Link } from 'react-router-dom';
+import authorImgSrc from '../assets/portrait-transparent.png';
+import githubImgSrc from '../assets/github.svg';
+import mailImgSrc from '../assets/mail.svg';
+import linkedInImgSrc from '../assets/linkedIn.png';
+import styles from '../styles/footer.module.css';
 
 const Footer = () => (
   <footer>
-    <nav>
-      <ul>
-        <li>
-          <a href="https://github.com/hannahkim313">GitHub</a>
-        </li>
-        <li>
-          <a href="mailto:devhkim13@gmail.com">Email</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/hannah-n-kim">LinkedIn</a>
-        </li>
-      </ul>
-    </nav>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-    <p>&copy; 2024 Hannah Kim. All rights reserved.</p>
+    <div className={styles.authorContainer}>
+      <img
+        className={styles.authorImg}
+        src={authorImgSrc}
+        alt="Digital illustration of Hannah Kim"
+      />
+      <p className={styles.authorInfo}>
+        <span>Hannah Kim</span>
+        <span>Creative Front-End Developer</span>
+      </p>
+    </div>
+    <div className={styles.navListContainer}>
+      <nav>
+        <h3 className="hidden">Main Navigation</h3>
+        <ul className={styles.navList}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <h3 className="hidden">Get In Touch</h3>
+        <ul className={`${styles.navList} ${styles.socials}`}>
+          <li>
+            <a
+              href="https://github.com/hannahkim313"
+              aria-label="Github profile"
+            >
+              <img src={githubImgSrc} alt="Github icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:devhkim13@gmail.com"
+              aria-label="Send an email to Hannah Kim"
+            >
+              <img src={mailImgSrc} alt="Mail icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/hannah-n-kim"
+              aria-label="LinkedIn profile"
+            >
+              <img src={linkedInImgSrc} alt="LinkedIn icon" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <p className={styles.copyright}>
+      &copy; 2024 Hannah Kim. All rights reserved.
+    </p>
   </footer>
 );
 
