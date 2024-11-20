2b5538cf374bf40dd1bf7613ce20b11c0513efd2
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-11-20 11:28:17 -0800

    Refactor project cards to only wrap anchors around the project thumbnail and title and add styles to the home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,7 @@ import urbanThreadImgSrc from '../assets/urban-thread-main.png';
 import battleshipImgSrc from '../assets/battleship-main.png';
 import Header from './Header';
 import Footer from './Footer';
+import styles from '../styles/home-page.module.css';
 
 const HomePage = () => {
   const location = useLocation();
@@ -12,62 +13,97 @@ const HomePage = () => {
   const isHome = location.pathname === '/';
 
   const mainContent = (
-    <main>
-      <img src={authorImgSrc} alt="Digital illustration of Hannah" />
-      <h1>Hi, I'm Hannah - Creative Front-End Developer</h1>
-      <p>
-        Building beautiful, responsive websites with a focus on user experience.
-      </p>
-      <h2>Featured Projects</h2>
-      <Link to="/projects/blog-api">
-        <article>
-          <h3>My Personal Blog API</h3>
-          <p>
-            A full-stack blog platform built with Prisma, PostgreSQL, and
-            Node/Express.js. This project features role-based authentication, a
-            secure comments system, and effective article management. Users can
-            view published articles while the author has the ability to create,
-            edit, and manage content dynamically.
+    <main className={styles.main}>
+      <section className={styles.authorSection}>
+        <img
+          className={styles.authorImg}
+          src={authorImgSrc}
+          alt="Digital illustration of Hannah Kim"
+        />
+        <div className={styles.authorDetails}>
+          <h1 className={styles.authorTitle}>Hi, I'm Hannah</h1>
+          <p className={styles.authorSummary}>
+            I'm a creative front-end developer who builds beautiful, responsive
+            websites with a focus on user experience.
           </p>
-          <img
-            src={apiImgSrc}
-            alt="Screenshot of the main configuration file displaying the API's overall structure."
-          />
-        </article>
-      </Link>
-      <Link to="/projects/mock-e-commerce-store">
-        <article>
-          <h3>Urban Thread: Mock E-Commerce Store</h3>
-          <p>
-            A responsive e-commerce application powered by the FakeStore API,
-            allowing users to browse products and manage items in their cart.
-            This project emphasizes a user-friendly design and delivers a
-            seamless shopping experience similar to that of real-world
-            e-commerce brands.
-          </p>
-          <img
-            src={urbanThreadImgSrc}
-            alt="Screenshot of the home page of Urban Thread featuring a navigation header, promotional banner, and 3 images of models representing the (fake) brand."
-          />
-        </article>
-      </Link>
-      <Link to="/projects/battleship">
-        <article>
-          <h3>Battleship Game</h3>
-          <p>
-            A web-based recreation of the classic Battleship game designed with
-            a simple user interface and interactive gameplay. This project
-            showcases algorithmic thinking, grid-based logic, and player-vs-AI
-            functionality for an engaging user experience.
-          </p>
-          <img
-            src={battleshipImgSrc}
-            alt="Screenshot of the gameplay featuring the player and opponent's game boards with hits and misses."
-          />
-        </article>
-      </Link>
-      <Link to="/contact">Get In Touch</Link>
-      <Link to="/projects">See All Projects</Link>
+        </div>
+      </section>
+      <hr className={styles.divider} />
+      <section className={styles.featuredProjects}>
+        <h2 className={styles.featuredProjectsTitle}>Featured Projects</h2>
+        <div className={styles.projectsContainer}>
+          <article className={styles.projectCard}>
+            <Link to="/projects/blog-api">
+              <div className={styles.projectThumbnailContainer}>
+                <img
+                  className={styles.projectThumbnail}
+                  src={apiImgSrc}
+                  alt="Screenshot of the Blog API's main configuration file."
+                />
+              </div>
+            </Link>
+            <Link to="/projects/blog-api">
+              <h3>My Personal Blog API</h3>
+            </Link>
+            <p>
+              A full-stack blog platform built with Prisma, PostgreSQL, and
+              Node/Express.js. This project features role-based authentication,
+              a secure comments system, and effective article management. Users
+              can view published articles while the author has the ability to
+              create, edit, and manage content dynamically.
+            </p>
+          </article>
+          <article className={styles.projectCard}>
+            <Link to="/projects/mock-e-commerce-store">
+              <div className={styles.projectThumbnailContainer}>
+                <img
+                  className={styles.projectThumbnail}
+                  src={urbanThreadImgSrc}
+                  alt="Screenshot of Urban Thread's home page."
+                />
+              </div>
+            </Link>
+            <Link to="/projects/mock-e-commerce-store">
+              <h3>Urban Thread: Mock E-Commerce Store</h3>
+            </Link>
+            <p>
+              A responsive e-commerce application powered by the FakeStore API,
+              allowing users to browse products and manage items in their cart.
+              This project emphasizes a user-friendly design and delivers a
+              seamless shopping experience similar to that of real-world
+              e-commerce brands.
+            </p>
+          </article>
+          <article className={styles.projectCard}>
+            <Link to="/projects/battleship">
+              <div className={styles.projectThumbnailContainer}>
+                <img
+                  className={styles.projectThumbnail}
+                  src={battleshipImgSrc}
+                  alt="Screenshot of a gameplay of Battleship."
+                />
+              </div>
+            </Link>
+            <Link to="/projects/battleship">
+              <h3>Battleship Game</h3>
+            </Link>
+            <p>
+              A web-based recreation of the classic Battleship game designed
+              with a simple user interface and interactive gameplay. This
+              project showcases algorithmic thinking, grid-based logic, and
+              player-vs-AI functionality for an engaging user experience.
+            </p>
+          </article>
+        </div>
+      </section>
+      <div className={styles.ctaContainer}>
+        <Link className="cta" to="/contact">
+          Get In Touch
+        </Link>
+        <Link className="cta-secondary" to="/projects">
+          See All Projects
+        </Link>
+      </div>
     </main>
   );
 
