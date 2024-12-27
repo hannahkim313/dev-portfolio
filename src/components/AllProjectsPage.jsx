f93237d5e1b2b443bc964a224051954c4dbe5719
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-12-27 10:23:38 -0800

    Refactor project card structure and add styles to AllProjectsPage

--- src/components/AllProjectsPage.jsx
+++ src/components/AllProjectsPage.jsx
@@ -314,103 +314,129 @@ const AllProjectsPage = () => (
     <section className={styles.projectsContainer}>
       <article className={styles.projectCard}>
         <Link to="/projects/blog-api" state={blogApiState}>
-          <img
-            src={apiImgSrc}
-            alt={`Screenshot of the main configuration file displaying the API's
+          <div className={styles.projectThumbnailContainer}>
+            <img
+              className={styles.projectThumbnail}
+              src={apiImgSrc}
+              alt={`Screenshot of the main configuration file displaying the API's
               overall structure.`}
-          />
+            />
+          </div>
         </Link>
-        <Link to="/projects/blog-api" state={blogApiState}>
-          <h2>My Personal Blog API</h2>
-        </Link>
-        <p>
-          A full-stack blog platform built with Prisma, PostgreSQL, and
-          Node/Express.js. This project features role-based authentication, a
-          secure comments system, and effective article management. Users can
-          view published articles while the author has the ability to create,
-          edit, and manage content dynamically.
-        </p>
+        <div className={styles.projectDetails}>
+          <Link to="/projects/blog-api" state={blogApiState}>
+            <h2>My Personal Blog API</h2>
+          </Link>
+          <p>
+            A full-stack blog platform built with Prisma, PostgreSQL, and
+            Node/Express.js. This project features role-based authentication, a
+            secure comments system, and effective article management. Users can
+            view published articles while the author has the ability to create,
+            edit, and manage content dynamically.
+          </p>
+        </div>
       </article>
       <article className={styles.projectCard}>
         <Link
           to="/projects/inventory-management-application"
           state={inventoryMateState}
         >
-          <img
-            src={inventoryMateImgSrc}
-            alt="Screenshot of the home page of InventoryMate featuring a navigational sidebar and an overview of the application in the main content."
-          />
+          <div className={styles.projectThumbnailContainer}>
+            <img
+              className={styles.projectThumbnail}
+              src={inventoryMateImgSrc}
+              alt="Screenshot of the home page of InventoryMate featuring a navigational sidebar and an overview of the application in the main content."
+            />
+          </div>
         </Link>
-        <Link
-          to="/projects/inventory-management-application"
-          state={inventoryMateState}
-        >
-          <h2>InventoryMate: Simple Inventory Management Application</h2>
-        </Link>
-        <p>
-          An inventory management application built with Express and MongoDB,
-          which features site-specific routes, EJS templates for dynamic views,
-          and an MVC architecture to ensure a clear separation of logic. This
-          project enables users to create, read, update, and delete (CRUD)
-          categories and items within a structured interface.
-        </p>
+        <div className={styles.projectDetails}>
+          <Link
+            to="/projects/inventory-management-application"
+            state={inventoryMateState}
+          >
+            <h2>InventoryMate: Inventory Management Application</h2>
+          </Link>
+          <p>
+            An inventory management application built with Express and MongoDB,
+            featuring site-specific routes, EJS templates for dynamic views, and
+            an MVC architecture. This project enables users to create, read,
+            update, and delete (CRUD) categories and items within a structured
+            interface.
+          </p>
+        </div>
       </article>
       <article className={styles.projectCard}>
         <Link to="/projects/mock-e-commerce-store" state={urbanThreadState}>
-          <img
-            src={urbanThreadImgSrc}
-            alt={`Screenshot of the home page of Urban Thread featuring a
+          <div className={styles.projectThumbnailContainer}>
+            <img
+              className={styles.projectThumbnail}
+              src={urbanThreadImgSrc}
+              alt={`Screenshot of the home page of Urban Thread featuring a
               navigation header, promotional banner, and 3 images of models
               representing the (fake) brand.`}
-          />
+            />
+          </div>
         </Link>
-        <Link to="/projects/mock-e-commerce-store" state={urbanThreadState}>
-          <h2>Urban Thread: Mock E-Commerce Store</h2>
-        </Link>
-        <p>
-          A responsive e-commerce application powered by the FakeStore API,
-          allowing users to browse products and manage items in their cart. This
-          project emphasizes a user-friendly design and delivers a seamless
-          shopping experience similar to that of real-world e-commerce brands.
-        </p>
+        <div className={styles.projectDetails}>
+          <Link to="/projects/mock-e-commerce-store" state={urbanThreadState}>
+            <h2>Urban Thread: Mock E-Commerce Store</h2>
+          </Link>
+          <p>
+            A responsive e-commerce application powered by the FakeStore API,
+            allowing users to browse products and manage items in their cart.
+            This project emphasizes a user-friendly design and delivers a
+            seamless shopping experience similar to that of real-world
+            e-commerce brands.
+          </p>
+        </div>
       </article>
       <article className={styles.projectCard}>
         <Link to="/projects/memory-card-game" state={memoryCardGameState}>
-          <img
-            src={memoryCardImgSrc}
-            alt={`Screenshot of the home page of the memory card game featuring a
+          <div className={styles.projectThumbnailContainer}>
+            <img
+              className={styles.projectThumbnail}
+              src={memoryCardImgSrc}
+              alt={`Screenshot of the home page of the memory card game featuring a
               scoreboard at the top and 8 cards with images of different Animal
               Crossing characters laid out below it.`}
-          />
+            />
+          </div>
         </Link>
-        <Link to="/projects/memory-card-game" state={memoryCardGameState}>
-          <h2>Memory Card Game: Interactive Memory Challenge</h2>
-        </Link>
-        <p>
-          An interactive memory card game developed with React to challenge
-          players' memory and recall abilities. This project utilizes hooks for
-          managing state and side effects while fetching card data from the
-          Nintendo Amiibo API, creating a smooth user experience as players
-          attempt to match pairs of cards.
-        </p>
+        <div className={styles.projectDetails}>
+          <Link to="/projects/memory-card-game" state={memoryCardGameState}>
+            <h2>Memory Card Game</h2>
+          </Link>
+          <p>
+            An interactive memory card game developed with React to challenge
+            players' memory and recall abilities. This project utilizes hooks
+            for managing state and side effects while fetching card data from
+            the Nintendo Amiibo API, creating a smooth user experience as
+            players attempt to match pairs of cards.
+          </p>
+        </div>
       </article>
       <article className={styles.projectCard}>
         <Link to="/projects/battleship" state={battleshipState}>
-          <img
-            src={battleshipImgSrc}
-            alt={`Screenshot of the gameplay featuring the player and opponent's
+          <div className={styles.projectThumbnailContainer}>
+            <img
+              className={styles.projectThumbnail}
+              src={battleshipImgSrc}
+              alt={`Screenshot of the gameplay featuring the player and opponent's
               game boards with hits and misses.`}
-          />
+            />
+          </div>
         </Link>
-        <Link to="/projects/battleship" state={battleshipState}>
-          <h2>Battleship Game</h2>
-        </Link>
-        <p>
-          A web-based recreation of the classic Battleship game designed with a
-          simple user interface and interactive gameplay. This project showcases
-          algorithmic thinking, grid-based logic, and player-vs-AI functionality
-          for an engaging user experience.
-        </p>
+        <div className={styles.projectDetails}>
+          <Link to="/projects/battleship" state={battleshipState}>
+            <h2>Battleship Game</h2>
+          </Link>
+          <p>
+            A web-based recreation of the classic Battleship game designed with
+            a simple user interface and interactive gameplay. This project
+            showcases algorithmic thinking, grid-based logic, and player-vs-AI
+            functionality for an engaging user experience.
+          </p>
+        </div>
       </article>
     </section>
     <p className={styles.githubCta}>
