9149c33044212aac550f4b04c87a7c30392a42e9
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-12-27 14:53:00 -0800

    Add an underline to the nav link in Header depending on the active page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Drawer, useMediaQuery } from '@mui/material';
 import menuImg from '../assets/menu.svg';
 import closeImg from '../assets/close.svg';
 import styles from '../styles/header.module.css';
 
 const Header = () => {
+  const location = useLocation();
+  const isLargeScreen = useMediaQuery('(min-width: 1024px)');
+
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const isLargeScreen = useMediaQuery('(min-width: 1024px)');
+  const currentPath = location.pathname.split('/')[1];
 
   const handleSidebarToggle = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -28,16 +31,36 @@ const Header = () => {
             <h3 className="hidden">Main Navigation</h3>
             <ul className={styles.navList}>
               <li>
-                <Link to="/">Home</Link>
+                <Link
+                  className={`${currentPath === '' ? styles.underline : ''}`}
+                  to="/"
+                >
+                  Home
+                </Link>
               </li>
               <li>
-                <Link to="/projects">Projects</Link>
+                <Link
+                  className={`${currentPath === 'projects' ? styles.underline : ''}`}
+                  to="/projects"
+                >
+                  Projects
+                </Link>
               </li>
               <li>
-                <Link to="/about">About</Link>
+                <Link
+                  className={`${currentPath === 'about' ? styles.underline : ''}`}
+                  to="/about"
+                >
+                  About
+                </Link>
               </li>
               <li>
-                <Link to="/blog">Blog</Link>
+                <Link
+                  className={`${currentPath === 'blog' ? styles.underline : ''}`}
+                  to="/blog"
+                >
+                  Blog
+                </Link>
               </li>
               <li>
                 <Link className="cta" to="/contact">
