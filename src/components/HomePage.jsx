a2ae018b18b9fe883c364580e39fbfcc3b557e5e
Hannah <67137839+hannahkim313@users.noreply.github.com>
2024-12-27 11:50:15 -0800

    Refactor project cards to a single component and move project data into a single file

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import purpleFlowerImgSrc from '../assets/flower-purple.png';
 import redFlowerImgSrc from '../assets/flower-red.png';
 import whiteFlowerImgSrc from '../assets/flower-white.png';
 import authorImgSrc from '../assets/portrait-transparent.png';
-import apiImgSrc from '../assets/api-main.png';
-import urbanThreadImgSrc from '../assets/urban-thread-main.png';
-import battleshipImgSrc from '../assets/battleship-main.png';
+import projects from '../data/projectsData';
 import Header from './Header';
 import Footer from './Footer';
 import styles from '../styles/home-page.module.css';
+import ProjectCard from './ProjectCard';
 
 const HomePage = () => {
   const location = useLocation();
@@ -80,74 +80,11 @@ const HomePage = () => {
       <section className={styles.featuredProjects}>
         <h2 className={styles.featuredProjectsTitle}>Featured Projects</h2>
         <div className={styles.projectsContainer}>
-          <article className={styles.projectCard}>
-            <Link to="/projects/blog-api">
-              <div className={styles.projectThumbnailContainer}>
-                <img
-                  className={styles.projectThumbnail}
-                  src={apiImgSrc}
-                  alt="Screenshot of the Blog API's main configuration file."
-                />
-              </div>
-            </Link>
-            <div className={styles.projectDetails}>
-              <Link to="/projects/blog-api">
-                <h3>My Personal Blog API</h3>
-              </Link>
-              <p>
-                A full-stack blog platform built with Prisma, PostgreSQL, and
-                Node/Express.js. This project features role-based
-                authentication, a secure comments system, and effective article
-                management. Users can view published articles while the author
-                has the ability to create, edit, and manage content dynamically.
-              </p>
-            </div>
-          </article>
-          <article className={styles.projectCard}>
-            <Link to="/projects/mock-e-commerce-store">
-              <div className={styles.projectThumbnailContainer}>
-                <img
-                  className={styles.projectThumbnail}
-                  src={urbanThreadImgSrc}
-                  alt="Screenshot of Urban Thread's home page."
-                />
-              </div>
-            </Link>
-            <div className={styles.projectDetails}>
-              <Link to="/projects/mock-e-commerce-store">
-                <h3>Urban Thread: Mock E-Commerce Store</h3>
-              </Link>
-              <p>
-                A responsive e-commerce application powered by the FakeStore
-                API, allowing users to browse products and manage items in their
-                cart. This project emphasizes a user-friendly design and
-                delivers a seamless shopping experience similar to that of
-                real-world e-commerce brands.
-              </p>
-            </div>
-          </article>
-          <article className={styles.projectCard}>
-            <Link to="/projects/battleship">
-              <div className={styles.projectThumbnailContainer}>
-                <img
-                  className={styles.projectThumbnail}
-                  src={battleshipImgSrc}
-                  alt="Screenshot of a gameplay of Battleship."
-                />
-              </div>
-            </Link>
-            <div className={styles.projectDetails}>
-              <Link to="/projects/battleship">
-                <h3>Battleship Game</h3>
-              </Link>
-              <p>
-                A web-based recreation of the classic Battleship game designed
-                with a simple user interface and interactive gameplay. This
-                project showcases algorithmic thinking, grid-based logic, and
-                player-vs-AI functionality for an engaging user experience.
-              </p>
-            </div>
-          </article>
+          {projects
+            .filter((project) => project.isFeatured)
+            .map((projectData) => (
+              <ProjectCard key={uuidv4()} data={projectData} />
+            ))}
         </div>
       </section>
       <div className={styles.ctaContainer}>
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import styles from '../styles/project-card.module.css';
+
+const ProjectCard = ({ data }) => (
+  <article className={styles.projectCard}>
+    <Link to={data.projectLink} state={data}>
+      <div className={styles.projectThumbnailContainer}>
+        <img
+          className={styles.projectThumbnail}
+          src={data.mainImg.src}
+          alt={data.mainImg.alt}
+        />
+      </div>
+    </Link>
+    <div className={styles.projectDetails}>
+      <Link to={data.projectLink} state={data}>
+        <h2>{data.title}</h2>
+      </Link>
+      <p>{data.description}</p>
+    </div>
+  </article>
+);
+
+export default ProjectCard;
