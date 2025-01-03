9ee35058c527870f04f9c2df29db18bd0d95d98e
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-01-02 16:02:39 -0800

    Update design elements, refactor UI links, and add styles to Blog
    This commit changes the page title and adds a subheading to the blog page. It also refactors article cards by wrapping only the article title in a link element as opposed to the entire article card.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useFetchAPI from '../hooks/useFetchAPI';
 import formatDate from '../utils/formatDate';
+import styles from '../styles/blog.module.css';
 
 const Blog = () => {
   const [loading, setLoading] = useState(true);
@@ -35,37 +36,45 @@ const Blog = () => {
 
   return (
     <main>
-      <h1>Hannah Kim's Developer Blog</h1>
-      <h2>Articles</h2>
-      {loading && <p>Loading articles...</p>}
-      {error && <p>{error}</p>}
-      {data && data.articles.length === 0 && (
-        <>
-          <p>No Articles Yet!</p>
-          <p>
-            It looks like there aren't any articles published at the moment. In
-            the meantime, feel free to check back later as I work hard to bring
-            you insightful content soon. Thank you for your patience!
-          </p>
-        </>
-      )}
-      {data &&
-        data.articles.length > 0 &&
-        data.articles.map((article) => {
-          return (
-            <Link
-              to={`/blog/articles/${article.url}`}
-              state={{ id: article.id }}
-              key={article.id}
-            >
-              <article>
+      <section className={`pageIntro ${styles.introSection}`}>
+        <h1>Personal Developer Blog</h1>
+        <p>
+          Insights on web development, current and future projects, and my
+          journey as a developer.
+        </p>
+      </section>
+      <section className={styles.articlesSection}>
+        <h2 className={`sectionHeading ${styles.articlesHeading}`}>Articles</h2>
+        {loading && <p>Loading articles...</p>}
+        {error && <p>{error}</p>}
+        {data && data.articles.length === 0 && (
+          <div className={styles.noArticlesContainer}>
+            <p className={styles.noArticlesHeading}>No Articles Yet!</p>
+            <p>
+              It looks like there aren't any articles published at the moment.
+              In the meantime, feel free to check back later as I work hard to
+              bring you insightful content soon. Thank you for your patience!
+            </p>
+          </div>
+        )}
+        {data &&
+          data.articles.length > 0 &&
+          data.articles.map((article) => {
+            return (
+              <article className={styles.articleCard} key={article.id}>
                 <p>{formatDate(article.createdAt)}</p>
-                <h3>{article.title}</h3>
+                <Link
+                  className="ctaSecondary"
+                  to={`/blog/articles/${article.url}`}
+                  state={{ id: article.id }}
+                >
+                  <h3 className="sectionHeading">{article.title}</h3>
+                </Link>
                 <p>{article.description}</p>
               </article>
-            </Link>
-          );
-        })}
+            );
+          })}
+      </section>
     </main>
   );
 };
