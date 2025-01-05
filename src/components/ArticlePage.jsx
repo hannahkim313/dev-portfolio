470e730b494455426781e929497dbf29f5630830
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-01-04 18:27:29 -0800

    Style ArticlePage

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import useFetchAPI from '../hooks/useFetchAPI';
 import formatDate from '../utils/formatDate';
+import styles from '../styles/article-page.module.css';
 
 const ArticlePage = () => {
   const [loading, setLoading] = useState(true);
@@ -42,15 +43,17 @@ const ArticlePage = () => {
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       {data && (
-        <article>
-          <p>
-            {`${data.article.author.firstName} ${data.article.author.lastName}`}
-          </p>
-          <p>{formatDate(data.article.createdAt)}</p>
-          <h1>{data.article.title}</h1>
-          <p>{data.article.description}</p>
+        <article className={`contentEnd ${styles.articleContainer}`}>
+          <section className={`mb-sm ${styles.introSection}`}>
+            <div className={styles.introHeading}>
+              <p>{formatDate(data.article.createdAt)}</p>
+              <h1>{data.article.title}</h1>
+            </div>
+            <p>{data.article.description}</p>
+          </section>
           {
             <div
+              className={styles.articleContentContainer}
               dangerouslySetInnerHTML={{
                 __html: DOMPurify.sanitize(data.article.content),
               }}
