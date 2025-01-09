b8ba3b4b748209cf67d237e2fc4f80fd7c55a6c6
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-01-08 16:17:34 -0800

    Remove unnecessary wrapping of fragment in ProjectPage

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -9,80 +9,76 @@ const ProjectPage = () => {
   const state = location.state;
 
   return (
-    <>
-      <main>
-        <section className={`mb-sm ${styles.introSection}`}>
-          <h1>{state.title}</h1>
-          <div className="textContainer-sm">
-            <p>Date Completed: {state.date}</p>
-            <br />
-            <p>{state.description}</p>
-          </div>
-          <div className={styles.projectThumbnailContainer}>
-            <img
-              className={styles.projectThumbnail}
-              src={state.mainImg.src}
-              alt={state.mainImg.alt}
-            />
-          </div>
-        </section>
-        <section className={styles.projectDetailsSection}>
-          <h2 className="hidden mb-xs">Project Details</h2>
-          <div className={`mb-xs ${styles.projectCtaContainer}`}>
-            {state.liveLink && (
-              <a
-                className={`cta ${styles.ctaCustom}`}
-                href={state.liveLink}
-                target="_blank"
-              >
-                View Demo
-              </a>
-            )}
+    <main>
+      <section className={`mb-sm ${styles.introSection}`}>
+        <h1>{state.title}</h1>
+        <div className="textContainer-sm">
+          <p>Date Completed: {state.date}</p>
+          <br />
+          <p>{state.description}</p>
+        </div>
+        <div className={styles.projectThumbnailContainer}>
+          <img
+            className={styles.projectThumbnail}
+            src={state.mainImg.src}
+            alt={state.mainImg.alt}
+          />
+        </div>
+      </section>
+      <section className={styles.projectDetailsSection}>
+        <h2 className="hidden mb-xs">Project Details</h2>
+        <div className={`mb-xs ${styles.projectCtaContainer}`}>
+          {state.liveLink && (
             <a
               className={`cta ${styles.ctaCustom}`}
-              href={state.sourceCode}
+              href={state.liveLink}
               target="_blank"
             >
-              View on GitHub
+              View Demo
             </a>
-          </div>
-          <div className="mb-xs textContainer-lg">
-            <h3 className={`mb-xs ${styles.techStackTitle}`}>
-              Technology Stack
-            </h3>
-            <ul className={styles.techStackList}>
-              {state.techStack.map((tech) => (
-                <li className={styles.techStackItem} key={uuidv4()}>
-                  {tech}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </section>
-        <section className={styles.keyFeaturesSection}>
-          <h2 className="mb-xs">Key Features</h2>
-          <ul className="textContainer-sm">
-            {state.features.map((feature) => (
-              <Fragment key={uuidv4()}>
-                <li className={styles.keyFeatureItem}>{feature}</li>
-                <br />
-              </Fragment>
+          )}
+          <a
+            className={`cta ${styles.ctaCustom}`}
+            href={state.sourceCode}
+            target="_blank"
+          >
+            View on GitHub
+          </a>
+        </div>
+        <div className="mb-xs textContainer-lg">
+          <h3 className={`mb-xs ${styles.techStackTitle}`}>Technology Stack</h3>
+          <ul className={styles.techStackList}>
+            {state.techStack.map((tech) => (
+              <li className={styles.techStackItem} key={uuidv4()}>
+                {tech}
+              </li>
             ))}
           </ul>
-        </section>
-        <div className={`contentEnd ${styles.ctaContainer}`}>
-          <Link className="cta" to="/contact">
-            Get In Touch
-          </Link>
-          <Link
-            className={`ctaSecondary ${styles.ctaSecondaryCustom}`}
-            to="/projects"
-          >
-            See All Projects
-          </Link>
         </div>
-      </main>
-    </>
+      </section>
+      <section className={styles.keyFeaturesSection}>
+        <h2 className="mb-xs">Key Features</h2>
+        <ul className="textContainer-sm">
+          {state.features.map((feature) => (
+            <Fragment key={uuidv4()}>
+              <li className={styles.keyFeatureItem}>{feature}</li>
+              <br />
+            </Fragment>
+          ))}
+        </ul>
+      </section>
+      <div className={`contentEnd ${styles.ctaContainer}`}>
+        <Link className="cta" to="/contact">
+          Get In Touch
+        </Link>
+        <Link
+          className={`ctaSecondary ${styles.ctaSecondaryCustom}`}
+          to="/projects"
+        >
+          See All Projects
+        </Link>
+      </div>
+    </main>
   );
 };
 
