0babb2c84ff3b5cfd0d13f35cdc1f95c141a1690
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-01-10 23:02:25 -0800

    Fix issue of env variables not being recognized in vite config by installing dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: import.meta.env.VITE_API_NODE_ENV === 'development',
+    sourcemap: process.env.VITE_API_NODE_ENV === 'development',
   },
 });
