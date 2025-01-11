33e0544b6ac58118dbce2497d6e8839e6f809b0a
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-01-10 22:01:34 -0800

    Add conditional configuration for sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false,
+    sourcemap: import.meta.env.VITE_NODE_ENV === 'development',
   },
 });
