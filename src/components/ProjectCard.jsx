24fdac19320ebd0154fc2922eddb5cc3a6c71fec
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-01-11 10:21:42 -0800

    Remove accidental data property on link elements in ProjectCard

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/project-card.module.css';
 
 const ProjectCard = ({ data }) => (
   <article className={styles.projectCard}>
-    <Link to={data.projectLink} data={data}>
+    <Link to={data.projectLink}>
       <div className={styles.projectThumbnailContainer}>
         <img
           className={styles.projectThumbnail}
